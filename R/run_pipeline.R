2649acce2c09d17bab8117adfc8def2607d2340a
Michael Vrána <vranami8@fit.cvut.cz>
2023-03-07 01:08:59 +0100

    Printing of stage inputs

--- R/run_pipeline.R
+++ R/run_pipeline.R
@@ -1,9 +1,11 @@
 library(rlang)
 library(purrr)
 
-create_metadata_function <- function(metadata_iters) function(stage_symbol) {
-    stage_name <- ensym(stage_symbol) %>% toString()
-    metadata_iters[[stage_name]]
+create_metadata_function <- function(metadata_iters) {
+    function(stage_symbol) {
+        stage_name <- ensym(stage_symbol) %>% toString()
+        metadata_iters[[stage_name]]
+    }
 }
 
 eval_inputs <- function(stage_results, input_quosures) {
@@ -16,7 +18,7 @@ eval_inputs <- function(stage_results, input_quosures) {
         eval_env <- new_environment(data = c(stage_results$results, dsl_funcs), parent = inputs_env)
 
         input <- eval(inputs_expr, envir = eval_env)
-        
+
         if (is_iter(input)) {
             return(input)
         }
@@ -33,10 +35,30 @@ stage_tasks_iter <- function(stage, input_iters) {
     }
 }
 
-run_pipeline <- function(pipeline, executor = r_executor, pipeline_dir = 'pipeline') {
+print_task_iter <- function(task_iter, idx = 0) {
+    if (task_iter$done) {
+        return()
+    }
+
+    cat("Input ", toString(idx), ":\n", sep = "")
+    cat("================================================================================\n")
+    str(task_iter$value$args)
+    cat("================================================================================\n\n")
+
+    print_task_iter(task_iter$next_iter(), idx + 1)
+}
+
+print_stage_inputs <- function(stage_name, task_iter) {
+    cat("Stage ", stage_name, " inputs:\n", sep = "")
+    print_task_iter(task_iter)
+}
+
+run_pipeline <- function(pipeline, executor = r_executor, pipeline_dir = "pipeline", print_inputs = FALSE) {
     reduce(pipeline$exec_order, function(stage_results, stage) {
         input_iters <- eval_inputs(stage_results, stage$input_quosures)
         task_iter <- stage_tasks_iter(stage, input_iters)
+
+        if (print_inputs) print_stage_inputs(stage$name, task_iter)
 
         stage_executor <- if (!is.null(stage$override_executor)) stage$override_executor else executor
 
