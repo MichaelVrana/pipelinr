c5d27529c73531101f15e3ef14e03b3109725f6f
Michael Vrána <vranami8@fit.cvut.cz>
2023-04-13 12:16:13 +0200

    Move SSH worker out of test

--- ssh_worker/run_test_workers.sh
+++ ssh_worker/run_test_workers.sh
@@ -3,13 +3,17 @@
 # This script runs the SSH worker containers and creates an SSH config together with GNU Parallel SSH login file
 # You can adjust the number of SSH workers in the docker-compose.yml file (replicas field)
 
+if [ ! -f ./id_rsa ]; then
+    ssh-keygen -t rsa -f ./id_rsa -N ''
+fi
+
 docker-compose up -d --build
 
-hostnames=$(docker network inspect test_worker_default | jq '.[0].Containers[] | "\(.Name) \(.IPv4Address)"' | sed -r 's/"|(\/.*)//g')
+hostnames=$(docker network inspect ssh_worker_default | jq '.[0].Containers[] | "\(.Name) \(.IPv4Address)"' | sed -r 's/"|(\/.*)//g')
 
 echo "Host *
     User worker
-    IdentityFile $(pwd)/id_rsa_test
+    IdentityFile $(pwd)/id_rsa
     StrictHostKeyChecking=no
     UserKnownHostsFile=/dev/null
     LogLevel ERROR
@@ -17,5 +21,3 @@ echo "Host *
 $(sed -r 's/(.*) (.*)/Host \1\n    Hostname \2\n/g' <<< $hostnames)" > ssh.config
 
 sed -r 's/(.*) .*/1\/ \1/g' <<< $hostnames > nodefile
-
-chmod 400 id_rsa_test
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$(cd tests/test_worker && ./run_test_workers.sh)
-PARALLEL_SSH="ssh -F $(pwd)/tests/test_worker/ssh.config" Rscript --vanilla --default-packages=devtools -e 'test()'
+$(cd ssh_worker && ./run_test_workers.sh)
+PARALLEL_SSH="ssh -F $(pwd)/ssh_worker/ssh.config" Rscript --vanilla --default-packages=devtools -e 'test()'
